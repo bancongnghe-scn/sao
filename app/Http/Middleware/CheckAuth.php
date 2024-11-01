b9ca360f9eb7eda329921d7438024e269a045581
DTViet-Sconnect <vietdt@s-connect.net>
2024-11-01 15:01:04 +0700

    build

--- app/Http/Middleware/CheckAuth.php
+++ app/Http/Middleware/CheckAuth.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 
 class CheckAuth
