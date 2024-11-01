f40886605ef112d5eacec99144b40c338f081d4d
DTViet-Sconnect <vietdt@s-connect.net>
2024-11-01 14:17:04 +0700

    loi vite

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,15 @@ export default defineConfig({
                 "resources/js/sao/strategy/build.js",
                 "resources/sass/training.scss",
                 "resources/js/rbac/api/apiMenu.js",
-
+                "resources/js/rbac/api/apiPermission.js",
+                "resources/js/rbac/api/apiRole.js",
+                "resources/js/rbac/menu.js",
+                "resources/js/rbac/permission.js",
+                "resources/js/rbac/role.js",
+                "resources/js/sao/strategy/vision-mission.js",
+                "resources/css/sao/strategy/vision-mission-company.css",
+                "resources/css/sao/strategy/vision-mission-unit.css",
+                "resources/css/sao/strategy/vision-mission.css",
                 // ========== comments ==========
                 "resources/css/comment.css",
                 "resources/js/comment/comment.js",
